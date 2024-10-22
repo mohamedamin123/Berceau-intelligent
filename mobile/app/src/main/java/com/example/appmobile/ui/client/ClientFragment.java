098b52254088969a7294f9fcc9456f9ed372f6a1
Mohamed Amin Gana <mohamedaming146@gmail.com>
2024-10-22 20:31:55 +0100

    en cour de developement

---
+++ mobile/app/src/main/java/com/example/appmobile/ui/client/ClientFragment.java
@@ -0,0 +1,38 @@
+package com.example.appmobile.ui.client;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.appmobile.databinding.FragmentClientBinding;
+
+public class ClientFragment extends Fragment {
+
+    private FragmentClientBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentClientBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.textClient.setText("Client");
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
