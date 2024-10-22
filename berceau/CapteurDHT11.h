40bb7319f02b521b9bdfda1ee633438250c61d06
Mohamed Amin Gana <mohamedaming146@gmail.com>
2024-10-22 21:48:11 +0100

    ajouter quelque fichier android et arduino

---
+++ berceau/CapteurDHT11.h
@@ -0,0 +1,5 @@
+
+void initCapteurDHT();      // Fonction pour initialiser le capteur DHT11
+void afficherTemperature(); // Fonction pour afficher la temperature
+void afficherHumidite();    // Fonction pour afficher l'humidite
+
